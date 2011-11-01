92ae2394460bde52135b6de90c59cbd779f58a14
Alex Gordon <alextgordon@gmail.com>
2011-11-01 02:37:07 +0000

    Context

---
+++ source/context.m
@@ -0,0 +1,50 @@
+#import "context.h"
+
+@implementation SCMContext
+
+
++ (id)sharedContext {
+    static id sharedContext;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        sharedContext = [[SCMContext alloc] init];
+    });
+    
+    return sharedContext;
+}
+- (id)init {
+    self = [super init];
+    if (self) {
+        statuses = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+- (NSString *)detectSCMRootForPath:(NSString *)path systemOut:(NSString**)systemOut {
+    
+    
+    
+}
+- (void)refreshAtRoot:(NSString *)root system:(NSString *)system {
+    [self clearItemsAtRoot:root];
+}
+
+- (NSString *)statusForPath:(NSString *)path {
+    return [statuses valueForKey:path];
+}
+- (void)clearItemsAtRoot:(NSString *)root {
+    root = [[root stringByStandardizingPath] stringByAppendingString:@"/"];
+    
+    NSMutableArray* keys = [NSMutableArray array];
+    for (NSString* key in statuses) {
+        if ([key hasPrefix:root])
+            [keys addObject:key];
+    }
+    [statuses removeObjectsForKeys:keys];
+}
+- (void)setStatus:(NSString *)status forPath:(NSString *)path {
+    
+    [statuses setValue:status forKey:path];
+}
+
+@end
